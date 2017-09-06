f2ee9144e79c5e22139fec72f7883534a0681476
Colin Gourlay <Gourlay.Colin@abc.net.au>
2017-09-06 12:41:07 +1000

    Extracted Bar component from Total component. Made total component functional.

--- src/components/Total/index.js
+++ src/components/Total/index.js
@@ -1,30 +1,26 @@
-const {h, Component} = require('preact');
+const {h} = require('preact');
 const CountTo = require('react-count-to').default;
+const Bar = require('../Bar');
 const styles = require('./Total.css');
 
 const MIN = 0;
 const MAX = 100;
 
-class Total extends Component {
-  render({label, groups, reducer, shouldTransition}) {
-    const pct = Math.min(MAX, Math.max(MIN, reducer(groups))); 
-    const previousPct = this._cachedPct || MIN;
+function Total({label, groups, reducer, shouldTransition}) {
+  const pct = Math.min(MAX, Math.max(MIN, reducer(groups))); 
+  const previousPct = this._cachedPct || MIN;
 
-    this._cachedPct = pct;
+  this._cachedPct = pct;
 
-    return (
-      <div className={styles.root}>
-        <div className={styles.value}>
-          <CountTo from={previousPct} to={pct} speed={shouldTransition ? 500 : 0} />%
-        </div>
-        {label}
-        <div className={styles.bar}>
-          <progress className={`${styles.fill} ${shouldTransition ? styles.shouldTransition : ''}`} value={pct} max={MAX} />
-          <progress className={`${styles.tick} ${shouldTransition ? styles.shouldTransition : ''}`} value={pct} max={MAX} />
-        </div>
+  return (
+    <div className={styles.root}>
+      <div className={styles.value}>
+        <CountTo from={previousPct} to={pct} speed={shouldTransition ? 500 : 0} />%
       </div>
-    );
-  }
+      {label}
+      <Bar value={pct} max={100} shouldTransition={shouldTransition} />
+    </div>
+  );
 }
 
 module.exports = Total;
