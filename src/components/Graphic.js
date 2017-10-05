b1eb81601d6d76577ba0a907583af4b6f70ce151
Colin Gourlay <Gourlay.Colin@abc.net.au>
2017-10-05 13:59:59 +1000

    Chart now displays yes response, factoring out dont-knows (like the total does).

--- src/components/Graphic.js
+++ src/components/Graphic.js
@@ -1,10 +1,10 @@
-const {h, Component} = require('preact');
+const { h, Component } = require('preact');
 const styled = require('styled-components').default;
 const Total = require('./Total');
 const Chart = require('./Chart');
 
 class Graphic extends Component {
-  constructor({data, scrollyteller}) {
+  constructor({ data, scrollyteller }) {
     super();
 
     this.onMark = this.onMark.bind(this);
@@ -22,7 +22,7 @@ class Graphic extends Component {
     };
 
     if (scrollyteller.activated) {
-      this.onMark({detail: scrollyteller});
+      this.onMark({ detail: scrollyteller });
     }
 
     document.addEventListener('mark', this.onMark);
@@ -32,13 +32,12 @@ class Graphic extends Component {
     document.removeEventListener('mark', this.onMark);
   }
 
-  onMark({detail}) {
+  onMark({ detail }) {
     if (!detail.activated) {
       return;
     }
 
-    const isDemo = typeof detail.activated.config.demo === 'boolean' ? 
-      detail.activated.config.demo : false;
+    const isDemo = typeof detail.activated.config.demo === 'boolean' ? detail.activated.config.demo : false;
 
     if (isDemo) {
       const u = String(detail.activated.config.u || 0);
@@ -53,8 +52,7 @@ class Graphic extends Component {
     }
 
     const id = detail.activated.config.id;
-    const isEditable = typeof detail.activated.config.editable === 'boolean' ? 
-      detail.activated.config.editable : false;
+    const isEditable = typeof detail.activated.config.editable === 'boolean' ? detail.activated.config.editable : false;
 
     this.loadRow(id, isEditable);
   }
@@ -79,31 +77,38 @@ class Graphic extends Component {
     });
   }
 
-  editGroup(key, x, y) {   
+  editGroup(key, x, y) {
     this.data[this.state.id][`x${key}`] = x;
     this.data[this.state.id][`y${key}`] = y;
     this.loadRow(this.state.id, this.state.isEditable);
   }
 
   toggleTransitions(shouldTransition) {
-    this.setState({shouldTransition});
+    this.setState({ shouldTransition });
   }
 
-  render({}, {groups, isDemo, isEditable, shouldTransition}) {
+  render({}, { groups, isDemo, isEditable, shouldTransition }) {
     return (
       <Container>
         <Total
           label={`of the response is ‘Yes’`}
           groups={groups}
           reducer={groupsToPct}
-          shouldTransition={shouldTransition} />
+          shouldTransition={shouldTransition}
+        />
         <Chart
-          groups={groups}
+          groups={groups.map(({ x, y, u, key, name }) => ({
+            x: x / (1 - u),
+            y,
+            key,
+            name
+          }))}
           isDemo={isDemo}
           isEditable={isEditable}
           shouldTransition={shouldTransition}
           toggleTransitions={this.toggleTransitions}
-          editGroup={this.editGroup} />
+          editGroup={this.editGroup}
+        />
       </Container>
     );
   }
@@ -111,27 +116,34 @@ class Graphic extends Component {
 
 function demoToGroups(u, x, y) {
   return rowToGroups({
-    p1: 1, p2: 1, p3: 1,
-    u1: u, u2: u, u3: u,
-    x1: x, x2: x, x3: x,
-    y1: y, y2: y, y3: y
-  }).concat([
-    {key: 4, name: 'All voters', p: 1, u: u, x, y}
-  ]);
+    p1: 1,
+    p2: 1,
+    p3: 1,
+    u1: u,
+    u2: u,
+    u3: u,
+    x1: x,
+    x2: x,
+    x3: x,
+    y1: y,
+    y2: y,
+    y3: y
+  }).concat([{ key: 4, name: 'All voters', p: 1, u: u, x, y }]);
 }
 
 function rowToGroups(row) {
   return [
-    {key: 1, name: 'Age 18-34', p: +row.p1, u: +row.u1, x: +row.x1, y: +row.y1},
-    {key: 2, name: 'Age 35-54', p: +row.p2, u: +row.u2, x: +row.x2, y: +row.y2},
-    {key: 3, name: 'Age  55+', p: +row.p3, u: +row.u3, x: +row.x3, y: +row.y3}
+    { key: 1, name: 'Age 18-34', p: +row.p1, u: +row.u1, x: +row.x1, y: +row.y1 },
+    { key: 2, name: 'Age 35-54', p: +row.p2, u: +row.u2, x: +row.x2, y: +row.y2 },
+    { key: 3, name: 'Age  55+', p: +row.p3, u: +row.u3, x: +row.x3, y: +row.y3 }
   ];
 }
 
 function groupsToPct(groups) {
-  return groups.length === 0 ? 0 :
-    groups.reduce((memo, group) => memo + group.p * (1 - group.u) * group.y * group.x / (1 - group.u), 0) /
-    groups.reduce((memo, group) => memo + group.p * (1 - group.u) * group.y, 0);
+  return groups.length === 0
+    ? 0
+    : groups.reduce((memo, group) => memo + group.p * (1 - group.u) * group.y * group.x / (1 - group.u), 0) /
+      groups.reduce((memo, group) => memo + group.p * (1 - group.u) * group.y, 0);
 }
 
 module.exports = Graphic;
@@ -140,5 +152,5 @@ const Container = styled.div`
   margin: auto;
   padding: 1rem;
   font-family: ${props => props.theme.fontSans};
-  font-size: .875rem;
+  font-size: 0.875rem;
 `;
